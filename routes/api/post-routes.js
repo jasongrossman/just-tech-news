bb2ba0d85c3fd762ef68bb8b54449e2de708ea6a
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-09-24 15:53:00 -0400

    added comments to posts

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -16,9 +16,17 @@ router.get('/', (req, res) => {
     order: [['created_at', 'DESC']],
     include: [
       {
-        model: User,
-        attributes: ['username']
-      }
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+            model: User,
+            attributes: ['username']
+        }
+      },
+    //   {
+    //     model: User,
+    //     attributes: ['username']
+    //   }
     ]
   })
     .then(dbPostData => res.json(dbPostData))
@@ -41,6 +49,14 @@ router.get('/:id', (req, res) => {
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+            model: User,
+            attributes: ['username']
+        }      
+      },
       {
         model: User,
         attributes: ['username']
