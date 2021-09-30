5b8293cbfe3bf4398ac28bd56d9134ef08201180
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-09-30 14:29:26 -0400

    added dashboard and post editing

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
-const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
   Comment.findAll()
@@ -11,11 +10,11 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', withAuth, (req, res) => {
+router.post('/', (req, res) => {
   // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
   Comment.create({
     comment_text: req.body.comment_text,
-    user_id: req.session.user_id,
+    user_id: req.body.user_id,
     post_id: req.body.post_id
   })
     .then(dbCommentData => res.json(dbCommentData))
@@ -25,7 +24,7 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
-router.delete('/:id', withAuth, (req, res) => {
+router.delete('/:id', (req, res) => {
   Comment.destroy({
     where: {
       id: req.params.id
